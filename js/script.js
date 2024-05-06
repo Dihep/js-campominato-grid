0e320814bded8a4cebb7a31b7380a3e8457b5497
Dihep <dihep.demayo@gmail.com>
2024-05-06 22:53:44 +0200

    M4- Modifica funzione creazione celle.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
-// Ricreazione della griglia 3x3 tramite JS
+// Creazione griglia
 const container = document.getElementById("container");
 for (let i = 0; i < 9; i++) {
     //Creazione nuova cella
     let cella = document.createElement("div");
-    cella.classList.add("cella");
-    cella.innerText = i+1;
+    //Stilizzazione cella
+    impostaCella(cella, i);
     //Evento click cella
-    cella.addEventListener("click", function() {
-    console.log("Click su cella ", i+1);
-    cella.classList.add("clicked");
+    cella.addEventListener("click", function () {
+        numCella = this.innerText;
+        console.log("Click su cella ", numCella);
+        this.classList.add("clicked");
     });
     //Aggiunta cella alla griglia
     container.append(cella);
+    console.log(`cella ${i + 1} creata.`);
 }
+
+//Funzioni
+function impostaCella(cella, i) {
+    cella.classList.add("cella");
+    console.log(cella, cella.classList);
+    cella.innerText = i + 1;
+    return(cella);
+}
+
+/*
+function clickCella() {
+    numCella = this.innerText;
+    console.log("Click su cella ", numCella);
+    this.classList.add("clicked");
+}
+*/
