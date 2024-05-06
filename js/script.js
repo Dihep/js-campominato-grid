2e881775c329f2a189937c604aa9bd73f7ab32bd
Dihep <dihep.demayo@gmail.com>
2024-05-06 22:02:37 +0200

    M2- Creazione griglia 3x3 tramite ciclo For in JS.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-// Creazione variabili
+// Ricreazione della griglia 3x3 tramite JS
+const container = document.getElementById("container");
+for (let i = 0; i < 9; i++) {
+    let cella = document.createElement("div");
+    cella.classList.add("cella");
+    container.append(cella);
+}
