a2680c7529c4e45423a31e14697792457bc934c1
Dihep <dihep.demayo@gmail.com>
2024-05-06 22:09:32 +0200

    M2- Numerazione celle.

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ const container = document.getElementById("container");
 for (let i = 0; i < 9; i++) {
     let cella = document.createElement("div");
     cella.classList.add("cella");
+    cella.innerText = i+1;
     container.append(cella);
 }
