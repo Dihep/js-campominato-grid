65c7f1658e7cd210ddd2741d0ebb68000d4c1a36
Dihep <dihep.demayo@gmail.com>
2024-05-06 22:15:40 +0200

    M3- Gestione cambio sfondo celle al click.

--- js/script.js
+++ js/script.js
@@ -7,7 +7,8 @@ for (let i = 0; i < 9; i++) {
     cella.innerText = i+1;
     //Evento click cella
     cella.addEventListener("click", function() {
-    console.log("Click su cella ", i+1);        
+    console.log("Click su cella ", i+1);
+    cella.classList.add("clicked");
     });
     //Aggiunta cella alla griglia
     container.append(cella);
