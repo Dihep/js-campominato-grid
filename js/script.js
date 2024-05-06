d87bfbecc865d944b63324c3a4c10f0489042429
Dihep <dihep.demayo@gmail.com>
2024-05-06 22:13:31 +0200

    M3- Gestione stampa al click della cella.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 // Ricreazione della griglia 3x3 tramite JS
 const container = document.getElementById("container");
 for (let i = 0; i < 9; i++) {
+    //Creazione nuova cella
     let cella = document.createElement("div");
     cella.classList.add("cella");
     cella.innerText = i+1;
+    //Evento click cella
+    cella.addEventListener("click", function() {
+    console.log("Click su cella ", i+1);        
+    });
+    //Aggiunta cella alla griglia
     container.append(cella);
 }
